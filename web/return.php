77f2f4900691fcd8df21cbc8fa42e7cbdf7e24bc
Ubuntu <azureuser@usc-lol.usc-lol.d6.internal.cloudapp.net>
2015-03-28 13:13:30 +0000

    updated web interface

--- web/return.php
+++ web/return.php
@@ -4,36 +4,93 @@
 
 <?php
 
+include("../db_settings.php");
 
+error_reporting(E_ERROR | E_PARSE);
 
+try {
 
-$servername = "localhost";
-$username = "";
-$password = "";
-$dbname = "measurement";
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = new PDO($db_url, $db_user, $db_pass, array(PDO::ATTR_PERSISTENT => true));
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-//echo "Connected successfully";
+    if(!isset($_GET["m_type"]) || !isset($_GET["dest_list"]) || !isset($_GET["interval"])){
+        throw new Exception('Missing parameter.');
+    }
 
-$measurement_type=$_GET["m_type"];
-$dest_list=$_GET["dest_list"];
-$interval=$_GET["interval"];
-$uuid=gen_uuid();
-$uuid=$uuid.'.m.ripeatlasdns.net';
+    $measurement_type = $_GET["m_type"];
+    
+    $dest_list = $_GET["dest_list"];
+    $dest_list = preg_replace('#\s+#',',', trim($dest_list)); //replace any newlines with commas
+    list($dest_valid, $dest_issue) = validate($dest_list);
+    if(!$dest_valid){
+        throw new Exception("Destination list must contain valid IPs or hostnames. '" . $dest_issue . "' is not valid.");
+    }
 
-$sql = "INSERT INTO measurement (measurement_type, destination_list, measurement_interval,hostname)
-VALUES ('$measurement_type', '$dest_list', '$interval','$uuid')";
+    $interval = $_GET["interval"];
+    $uuid = gen_uuid();
 
-if ($conn->query($sql) === TRUE) {
-echo 'Hostname: '.$uuid;
-//    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    $hostname = $uuid.'.m.ripeatlasdns.net';
+
+    //$sql = "INSERT INTO measurement (measurement_type, destination_list, measurement_interval,hostname) VALUES ('$measurement_type', '$dest_list', '$interval','$uuid')";
+
+    $stmt = $conn->prepare("INSERT INTO measurement (measurement_type, destination_list, measurement_interval, hostname) VALUES (:mtype, :dlist, :minterval, :hostname)");
+
+    $stmt->bindParam(':mtype', $measurement_type, PDO::PARAM_INT);
+    $stmt->bindParam(':dlist', $dest_list, PDO::PARAM_STR);
+    $stmt->bindParam(':minterval', $interval, PDO::PARAM_INT);
+    $stmt->bindParam(':hostname', $hostname, PDO::PARAM_STR, 100);
+
+    $result = $stmt->execute();
+
+    if ($result) {
+        echo 'Hostname: '.$hostname;
+    } else {
+        echo 'Unknown Error <br>';
+    }
+
+    $stmt->closeCursor();
+
+} catch (Exception $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+} finally {
+    if($conn){
+        $conn = null;
+    }
+}
+
+function validate($dests)
+{
+    //split the comma delimited string into an array
+    $dest_array = explode(",", $dests);
+
+    foreach ($dest_array as $dest) {
+        if(!inet_pton($dest) && !fqdn($dest)){
+            //if not a valid IP or fully qualified domain name, return false
+            return array(false, $dest);
+        }
+    }
+
+    return array(true, '');
+}
+
+function fqdn($domain_name)
+{
+    
+    $label_array = explode(".", $domain_name);
+
+    $domain_count = count($label_array);
+    if($domain_count < 2){
+        return false;
+    }
+
+    $tld = array_pop($label_array);
+    if(strlen($tld) < 2 || !ctype_alpha($tld)){
+        return false;
+    }
+
+    return preg_match("/^([a-z\d](-*[a-z\d])*)(\.([a-z\d](-*[a-z\d])*))*$/i", $domain_name); //valid chars check
+    //        && preg_match("/^.{1,253}$/", $domain_name)   //overall length check
+    //        && preg_match("/^[^\.]{1,63}(\.[^\.]{1,63})*$/", $domain_name)   ); //length of each label
 }
 
 function gen_uuid() {
